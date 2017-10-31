b4fcb210e5400ed02b355400bdb9a1c0de05e96e
nateHan <nhanak01@dcs.bbk.uk>
2017-10-31 13:42:45 -0500

    Shortening method

--- ButtonListener/buttonJs.js
+++ ButtonListener/buttonJs.js
@@ -1,7 +1,3 @@
 document.getElementById("buttonChange").addEventListener("click", function(){
-    if (document.body.style.background !== "red") {
-      document.body.style.background = "red";
-    } else {
-      document.body.style.background = "blue";
-    }
-  });
+  document.body.classList.toggle("backgrnd");
+});
